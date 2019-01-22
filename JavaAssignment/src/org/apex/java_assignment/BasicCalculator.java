c083130412edbfaa7a79082c4f93f5bfadec6e7b
knisha19 <nisha.sahay1905@gmail.com>
2019-01-22 12:14:43 -0800

    Changes made in the program

--- JavaAssignment/src/org/apex/java_assignment/BasicCalculator.java
+++ JavaAssignment/src/org/apex/java_assignment/BasicCalculator.java
@@ -1,27 +1,16 @@
 package org.apex.java_assignment;
-
 public class BasicCalculator {
-int num1;
-int num2;
-public int addition(int result) {
-	return result=num1+num2;
+	
+public int addition(int n1,int n2) {
+	return n1+n2;
 }
-public int subtraction(int result) {
-	if(num1>num2) {
-		 return result=num1-num2;
-	}
-		else {
-			return result=num2-num1;}
+public int subtraction(int n1,int n2) {
+	return n1-n2;
 }
-
-public int multiplication(int result) {
-	return num1*num2;
+public int multiplication(int n1,int n2) {
+	return n1*n2;
 }
-public int division(int result) {
-	if(num1>num2) {
-		 return result=num1/num2;
-	}
-		else {
-			return result=num2/num1;}
+public int division(int n1,int n2) {
+	return n1/n2;
 }
 }
--- JavaAssignment/src/org/apex/java_assignment/Calculator.java
+++ JavaAssignment/src/org/apex/java_assignment/Calculator.java
@@ -6,16 +6,36 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		BasicCalculator cal=new BasicCalculator();
+		int result = 0;
 		Scanner s=new Scanner(System.in);
+		System.out.println("Please choose between + - * and /: ");
+		String op=s.nextLine();
 		System.out.println("Enter the first number: ");
 		int num1=s.nextInt();
 		System.out.println("Enter the second number: ");
 		int num2=s.nextInt();
-		int r = 0;
-		System.out.println("C");
-		cal.addition(r);
-		cal.division(r);
-		
+            switch (op) {
+                case "+":
+                    result = cal.addition(num1, num2);
+                    System.out.println("Result= "+ result);
+                    break;
+                case "-":
+                    result = cal.subtraction(num1, num2);
+                    System.out.println("Result= "+ result);
+                    break;
+                case "*":
+                    result = cal.multiplication(num1, num2);
+                    System.out.println("Result= "+ result);
+                    break;
+                case "/":
+                    result = cal.division(num1, num2);
+                    System.out.println("Result= "+ result);
+                    break;
+                default:
+                    System.out.println("There is NO such operation! Please choose between the 4!");
+                    break;
+            }
+            
 	}
-
 }
+
